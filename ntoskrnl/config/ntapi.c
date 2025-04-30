bc8396f2d34db9dc7de3af5050c0c5b11a4d2804
Mohammad Amin Mollazadeh <madamin@pm.me>
2025-05-01 01:33:58 +0330

    [NTOS:CM] fix: minor mistakes with major effects

--- ntoskrnl/config/ntapi.c
+++ ntoskrnl/config/ntapi.c
@@ -1663,7 +1663,7 @@ NtNotifyChangeMultipleKeys(IN HANDLE MasterKeyHandle,
 
         /* Link notify block to the hive */
         Hive = CONTAINING_RECORD(KeyObject->KeyControlBlock->KeyHive, CMHIVE, Hive);
-        InsertHeadList(&(NotifyBlock->HiveList), &(Hive->NotifyList));
+        InsertHeadList(&(Hive->NotifyList), &(NotifyBlock->HiveList));
     }
     else
     {
