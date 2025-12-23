1b9dfdc853e41962e2bb2abae3a5c99a760ce0c9
Mikhail Tyukin <mishakeys20@gmail.com>
2025-12-23 18:58:52 -0500

    [PCIIDEDEX] disable spam

--- drivers/storage/ide/pciidex/chipset/ahci_io.c
+++ drivers/storage/ide/pciidex/chipset/ahci_io.c
@@ -381,7 +381,7 @@ AtaAhciPortHandleInterrupt(
         {
             AHCI_PORT_WRITE(ChanData->IoBase, PxSataNotification, Message);
 
-            WARN("CH %lu: Notification %08lx arrived\n", ChanData->Channel, Message);
+            //WARN("CH %lu: Notification %08lx arrived\n", ChanData->Channel, Message);
 
             ChanData->PortNotification(AtaAsyncNotificationDetected, ChanData->PortContext, 0x1);
         }
